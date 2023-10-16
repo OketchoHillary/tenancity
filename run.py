f81248e8c6746fe066312e480ddd8b3486ba13dd
Hillary Oketcho <larryoketcho@gmail.com>
2023-10-16 16:01:23 +0300

    env set up on run

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from decouple import config as env
 
 from models.auth import User
 from models.core import Business, Property
@@ -8,9 +9,10 @@ from utils import migrate, db
 from apps.auth.routes import auth_bp
 from apps.core.routes import core_bp
 
+
 def create_app():
     myapp = Flask(__name__)
-    myapp.config.from_object('config.DevelopmentConfig')
+    myapp.config.from_object(env('APP_SETTINGS'))
     myapp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     myapp.register_blueprint(core_bp)
     myapp.register_blueprint(auth_bp, url_prefix='/accounts')
